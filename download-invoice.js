edc57c207c3d8b0a7a77d1cd382f8c0fc1d46aee
Sohaib Alslamat <143258559+suhayp-gamma@users.noreply.github.com>
2024-01-04 16:18:07 +0300

    Create download-invoice.js

---
+++ download-invoice.js
@@ -0,0 +1,17 @@
+ window.addEventListener('message', function (event) {
+          try {
+            const data =
+              JSON.parse(event.data)
+              
+            if (data.data.url) {
+              const url = data.data.url
+              const link = document.createElement('a');
+              link.href = url;
+              link.target = '_blank';
+              link.download = 'The invoice';
+              document.body.appendChild(link);
+              link.click();
+              document.body.removeChild(link);
+            }
+          } catch {}
+        });
